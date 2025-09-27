e0e236e94d86f4cde2ee412dbd070b49c90f759d
sashank <sashanksai1064@gmail.com>
2025-09-26 22:05:42 -0700

    special vars

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -4,7 +4,7 @@ NUMBER=$1
 
 if [ $NUMBER -lt 10 ]
 then
-    echo " the $NUMBER is less than 10"
+echo " the $NUMBER is less than 10"
 else
-    echo " the $NUMBER is not lessthan 10"
+echo " the $NUMBER is not lessthan 10"
 fi
