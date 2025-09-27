eb34ef65607c89884245f1fc6380545212589d0f
sashank <sashanksai1064@gmail.com>
2025-09-26 21:42:30 -0700

    special vars

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -3,3 +3,10 @@
 
 echo "all variables passed to the script: $@"
 echo " number off variables: $#"
+echo "Script name: $0"
+echo "curent directory: $PWD"
+echo "user runnin gthis acript: $USER"
+echo "home directory oif the user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of the last command in background: $!"
